4e16d5e14a8714f3680a6e378a18febad702feb9
allisonchuang <allison.p.chuang.19@dartmouth.edu>
2017-05-15 00:22:19 -0400

    fixed surge

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'https://allisonchuang-lab5.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
-
+const ROOT_URL = 'https://allisonchuang-lab5p2.herokuapp.com/api';
 
 // keys for actiontypes
 export const ActionTypes = {
